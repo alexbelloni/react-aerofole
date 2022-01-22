966940959c842a8ace8726001a79a54d5518aa52
alexbelloni <alexbelloni@outlook.com>
2022-01-22 17:56:13 -0500

    readme with code block

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import './style.css';
 import { useState } from 'react';
 import React from 'react';
 
-const Aerofole = ({title, background, color, children}) => {
+const Aerofole = ({ title, background, color, children }) => {
     const [open, setOpen] = useState(false);
 
-    const handleClick = ()=>{
+    const handleClick = () => {
         setOpen(!open)
     }
 
     return (
         <div className="accordion">
-            <div className="title" style={{color, background}} onClick={handleClick}>
+            <div className="title" style={{ color, background }} onClick={handleClick}>
                 <span>{title}</span>
-                <span className="more">{!open && '. . .' }</span>
+                <span className="more">{!open && '. . .'}</span>
             </div>
             {open && children}
         </div>
